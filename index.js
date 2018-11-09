9fac2e746e9a5a18031ae6fcccdb926700636fd2
Carlo Bruno <carlofbruno@gmail.com>
2018-11-09 08:48:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ function takeANumber(line, name) {
 }
 
 function nowServing(line) {
-  if (line.length === 0) return 
+  if (line.length === 0) return "The line is currently empty.";
+  else {
+    return currently
+  }
 }
