102489117fe03ede37a36079cec803f3b684a529
Carlo Bruno <carlofbruno@gmail.com>
2018-11-09 08:57:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function nowServing(line) {
 function currentLine(line) {
   if (line.length === 0) {return "The line is currently empty."}
   else {
+    let people;
     for (let i=0; i < line.length; i++) {
       return i;
     }
